1e56475bde6b029127b43f1af196640faa0c8591
tgellai <78268336+tgellai@users.noreply.github.com>
2022-12-27 19:14:45 +0100

    Create script-6.sh
    A 6. feladat elvégezve!

---
+++ script-6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Adjon meg egy fájl vagy könyvtár elérést a vizsgálathoz! " FILE
+
+if [ -f $FILE ]
+then
+    echo "A megadott elérésen egy általános fájl található."
+elif [ -d $FILE ]
+then
+    echo "A megadott elérésen egy könyvtár található."
+else
+    echo "A megadott elérésen egy egyéb fájltípus található."
+fi
